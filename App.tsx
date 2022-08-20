3d6f91eaac79f4a67d4530b655e31152f4925de3
Ivan Duran <iamivanduran02@gmail.com>
2022-08-19 22:50:45 -0400

    added a timer to see how long you are in queue for

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button } from "react-native";
 import * as ScreenOrientation from "expo-screen-orientation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import * as NavigationBar from "expo-navigation-bar";
 import * as Device from "expo-device";
 
@@ -12,34 +12,108 @@ export default function App() {
     );
   }
 
+  const [pressed, setPressed] = useState(true);
+  const device = Device.osName; // Android: "Android"; iOS: "iOS" or "iPadOS"; web: "iOS", "Android", "Windows"
+
+  const [time, setTime] = useState(0);
+
+  const handlePress = () => {
+    setPressed(!pressed);
+    console.log(pressed);
+    if (!pressed) {
+      setTime(0);
+    }
+  };
+
   useEffect(() => {
     changeScreenOrientation();
-    if (Device.osName === "Android") {
+    if (device === "Android") {
       // only runs for android
       NavigationBar.setVisibilityAsync("hidden"); // hides navigation bar by default
     }
+    if (pressed) {
+      const interval = setInterval(() => {
+        setTime((prev) => prev + 1);
+      }, 1000);
+      console.log(time);
+
+      return () => {
+        clearInterval(interval);
+      };
+    }
   }, []);
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>
-        Open up App.tsx to start working on your app!
-      </Text>
-      <StatusBar hidden />
-    </View>
-  );
+  if (device === "iOS") {
+    return (
+      <View style={styles.container}>
+        {pressed ? (
+          <>
+            <Text style={styles.text}>
+              You are in matchmaking for {time} seconds
+            </Text>
+            <Button
+              title="Leave Matchmaking"
+              color="#fff"
+              onPress={() => {
+                handlePress();
+              }}
+            />
+          </>
+        ) : (
+          <Button
+            title="Join Matchmaking"
+            color="#fff"
+            onPress={() => {
+              handlePress();
+            }}
+          />
+        )}
+
+        <StatusBar hidden />
+      </View>
+    );
+  }
+  if (device === "Android") {
+    return (
+      <View style={styles.container}>
+        {pressed ? (
+          <>
+            <Text style={styles.text}>
+              You are in matchmaking for {time} seconds
+            </Text>
+            <Button
+              title="Leave Matchmaking"
+              color="#171717"
+              onPress={() => {
+                handlePress();
+              }}
+            />
+          </>
+        ) : (
+          <Button
+            title="Join Matchmaking"
+            color="#171717"
+            onPress={() => {
+              handlePress();
+            }}
+          />
+        )}
+
+        <StatusBar hidden />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "white",
+    backgroundColor: "#171717",
     width: "100%",
     height: "100%",
     alignItems: "center",
     justifyContent: "center",
-    border: "10px solid #fff",
   },
   text: {
-    color: "black",
+    color: "#fff",
   },
 });
