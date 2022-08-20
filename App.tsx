69cef57a00788147161c1ee695f00257ea9878f8
Ivan Duran <iamivanduran02@gmail.com>
2022-08-19 20:24:23 -0400

    installed some packages from expo so that I could hide android navbar etc.

--- App.tsx
+++ App.tsx
@@ -2,6 +2,8 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import * as ScreenOrientation from "expo-screen-orientation";
 import { useEffect } from "react";
+import * as NavigationBar from "expo-navigation-bar";
+import * as Device from "expo-device";
 
 export default function App() {
   async function changeScreenOrientation() {
@@ -9,13 +11,20 @@ export default function App() {
       ScreenOrientation.OrientationLock.LANDSCAPE
     );
   }
+
   useEffect(() => {
     changeScreenOrientation();
+    if (Device.osName === "Android") {
+      // only runs for android
+      NavigationBar.setVisibilityAsync("hidden"); // hides navigation bar by default
+    }
   }, []);
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>
+        Open up App.tsx to start working on your app!
+      </Text>
+      <StatusBar hidden />
     </View>
   );
 }
@@ -23,8 +32,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "white",
+    width: "100%",
+    height: "100%",
     alignItems: "center",
     justifyContent: "center",
+    border: "10px solid #fff",
+  },
+  text: {
+    color: "black",
   },
 });
